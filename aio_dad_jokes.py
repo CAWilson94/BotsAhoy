578be7b86544b4fb7b7614a5dcac97ce913b54f0
CAWilson94 <charlotteawilson.94@gmail.com>
2020-06-09 14:28:21 +0100

    shite advice stuff

--- aio_dad_jokes.py
+++ aio_dad_jokes.py
@@ -8,12 +8,20 @@ from token_bot import DAD_JOKE_TOKEN
 bot = commands.Bot(command_prefix='!')
 
 dad_joke_kingdom = "https://icanhazdadjoke.com/"
+shite_advice_kingdom = "https://api.adviceslip.com/advice"
+
 HEADERS = {"Accept": "application/json", "User-Agent": "Yer Maw"}
 
 async def dad_jokko(): 
     async with aiohttp.ClientSession() as session: 
         async with session.get(url=dad_joke_kingdom, headers=HEADERS) as response_boi:             
             return await response_boi.json()
+
+async def shite_advice(): 
+    async with aiohttp.ClientSession() as session: 
+        async with session.get(url=shite_advice_kingdom, headers=HEADERS) as response_boi:             
+            return await response_boi.json()
+
 
 def dad_joke_runner():
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())   
@@ -25,6 +33,12 @@ def dad_joke_runner():
 async def dad_joke(ctx): 
     json = await dad_jokko()
     await ctx.send(json['joke'])
+
+@bot.command(name='advice')
+@commands.cooldown(1, 10, commands.BucketType.user)
+async def advice_please(ctx): 
+    json = await shite_advice()
+    await ctx.send(json['slip']['advice'])
 
 
 @bot.event
