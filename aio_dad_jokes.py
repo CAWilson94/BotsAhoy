5972650f7326e578487b9eb7a5e6725ddac76d0b
CAWilson94 <charlotteawilson.94@gmail.com>
2020-06-09 01:37:48 +0100

    amazing bot amazing jokes ok dads

---
+++ aio_dad_jokes.py
@@ -0,0 +1,34 @@
+import aiohttp
+import asyncio
+
+import os
+from discord.ext import commands
+from token import DAD_JOKE_TOKEN
+
+bot = commands.Bot(command_prefix='!')
+
+dad_joke_kingdom = "https://icanhazdadjoke.com/"
+HEADERS = {"Accept": "application/json", "User-Agent": "Yer Maw"}
+
+async def dad_jokko(): 
+    async with aiohttp.ClientSession() as session: 
+        async with session.get(url=dad_joke_kingdom, headers=HEADERS) as response_boi:             
+            return await response_boi.json()
+
+def dad_joke_runner():
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())   
+    bob = asyncio.run(dad_jokko())        
+    print(bob['joke'])    
+
+@bot.command(name='joke')
+@commands.cooldown(1, 10, commands.BucketType.user)
+async def dad_joke(ctx): 
+    json = await dad_jokko()
+    await ctx.send(json['joke'])
+
+
+@bot.event
+async def on_ready(): 
+    print(f'{bot.user.name} has connected to Discord!')
+
+bot.run(DAD_JOKE_TOKEN)
