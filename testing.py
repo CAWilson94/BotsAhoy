6c911f3260bb64b24fe5d9110d7266d4cb2b50fc
CAWilson94 <charlotteawilson.94@gmail.com>
2020-06-09 14:39:49 +0100

    testing advice thangs

--- testing.py
+++ testing.py
@@ -1,1 +1,7 @@
 import requests 
+
+HEADERS = {"Accept": "application/json", "User-Agent": "Yer Maw"}
+
+r = requests.get("https://api.adviceslip.com/advice", headers=HEADERS)
+
+print(r.json()['slip']['advice'])
